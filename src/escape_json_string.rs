6411ef800b281e8561654113ae9a428c77fc3a09
amigin <amigin@gmail.com>
2022-02-06 16:59:38 +0200

    First Commit

---
+++ src/escape_json_string.rs
@@ -0,0 +1,83 @@
+const SINGLE_QUOTE: u8 = '\'' as u8;
+const DOUBLE_QUOTE: u8 = '"' as u8;
+const BACK_SLASH: u8 = '\\' as u8;
+
+pub struct EscapedJsonString<'s> {
+    as_slice: Option<&'s str>,
+    as_string: Option<String>,
+}
+
+impl<'s> EscapedJsonString<'s> {
+    pub fn new(src: &'s str) -> Self {
+        if !has_to_escape(src.as_bytes()) {
+            return Self {
+                as_slice: Some(src),
+                as_string: None,
+            };
+        }
+
+        let mut as_string = String::new();
+
+        for b in src.as_bytes() {
+            match *b {
+                DOUBLE_QUOTE => {
+                    as_string.push_str("\\\"");
+                }
+                BACK_SLASH => {
+                    as_string.push_str("\\\\");
+                }
+                SINGLE_QUOTE => {
+                    as_string.push_str("\\\'");
+                }
+                _ => {
+                    as_string.push(*b as char);
+                }
+            }
+        }
+
+        Self {
+            as_slice: None,
+            as_string: Some(as_string),
+        }
+    }
+
+    pub fn as_str(&'s self) -> &'s str {
+        if let Some(as_slice) = self.as_slice {
+            return as_slice;
+        }
+
+        if let Some(as_string) = &self.as_string {
+            return as_string;
+        }
+
+        panic!("EscapedJsonString is not initialized properly");
+    }
+}
+
+fn has_to_escape(src: &[u8]) -> bool {
+    for i in 0..src.len() {
+        if src[i] == '"' as u8 || src[i] == '\\' as u8 || src[i] == '\'' as u8 {
+            return true;
+        }
+    }
+
+    false
+}
+
+#[cfg(test)]
+mod test {
+    use crate::escape_json_string::*;
+
+    #[test]
+    pub fn test_basic_escape() {
+        let src = "Test String \\ \"MyData\" '";
+
+        let escaped = EscapedJsonString::new(src);
+
+        println!("{}", escaped.as_str());
+
+        assert_eq!("Test String \\\\ \\\"MyData\\\" \\'", escaped.as_str());
+
+        assert_eq!(src.len(), escaped.as_str().len() - 4);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+mod escape_json_string;
+mod json_object_writer;
+pub use escape_json_string::EscapedJsonString;
+pub use json_object_writer::JsonObjectWriter;
