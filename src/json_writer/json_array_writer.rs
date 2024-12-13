aa8687d009b29be7dff02525cb76228287b9b29a
amigin <amigin@gmail.com>
2024-12-13 18:42:39 +0200

    JsonWriter Optimizations

--- src/json_writer/json_array_writer.rs
+++ src/json_writer/json_array_writer.rs
@@ -1,7 +1,7 @@
-use super::JsonObject;
+use super::{JsonObject, JsonObjectWriter};
 
 pub struct JsonArrayWriter {
-    raw: Vec<u8>,
+    raw: Option<Vec<u8>>,
     first_element: bool,
 }
 
@@ -11,8 +11,16 @@ impl JsonArrayWriter {
         raw.push(b'[');
 
         Self {
-            raw,
+            raw: Some(raw),
             first_element: true,
+        }
+    }
+
+    pub(crate) fn new_as_nested(mut raw: Vec<u8>) -> Self {
+        raw.push(b'[');
+        Self {
+            first_element: true,
+            raw: Some(raw),
         }
     }
 
@@ -20,27 +28,45 @@ impl JsonArrayWriter {
         if self.first_element {
             self.first_element = false;
         } else {
-            self.raw.push(b',');
+            self.raw.as_mut().unwrap().push(b',');
         }
     }
 
     pub fn write_null_element(&mut self) {
         self.add_delimiter();
-        self.raw.extend_from_slice("null".as_bytes());
+        self.raw
+            .as_mut()
+            .unwrap()
+            .extend_from_slice("null".as_bytes());
     }
 
     pub fn build(mut self) -> Vec<u8> {
-        self.raw.push(b']');
-        self.raw
+        let mut raw = self.raw.take().unwrap();
+        raw.push(b']');
+        raw
     }
 
     pub fn write(&mut self, value: impl JsonObject) {
         self.add_delimiter();
-        value.write_into(&mut self.raw);
+        let raw = self.raw.as_mut().unwrap();
+        value.write_into(raw);
+    }
+
+    pub fn write_json_object(&mut self, write_object: impl Fn(&mut JsonObjectWriter) -> ()) {
+        self.add_delimiter();
+
+        let raw = self.raw.take().unwrap();
+
+        let mut json_object_writer = JsonObjectWriter::new_as_nested(raw);
+
+        write_object(&mut json_object_writer);
+
+        let raw = json_object_writer.build();
+        self.raw = Some(raw);
     }
 
     pub fn build_into(&self, dest: &mut Vec<u8>) {
-        dest.extend_from_slice(&self.raw);
+        dest.extend_from_slice(self.raw.as_ref().unwrap());
         dest.push(b']');
     }
 }
@@ -48,5 +74,31 @@ impl JsonArrayWriter {
 impl JsonObject for JsonArrayWriter {
     fn write_into(&self, dest: &mut Vec<u8>) {
         self.build_into(dest)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_array_write() {
+        let mut json_array_writer = super::JsonArrayWriter::new();
+
+        json_array_writer.write_json_object(|json_object| {
+            json_object.write("key1", "value1");
+            json_object.write("key2", "value2");
+        });
+
+        json_array_writer.write_json_object(|json_object| {
+            json_object.write("key1", "value1");
+            json_object.write("key2", "value2");
+        });
+
+        let result = json_array_writer.build();
+
+        assert_eq!(
+            result,
+            b"[{\"key1\":\"value1\",\"key2\":\"value2\"},{\"key1\":\"value1\",\"key2\":\"value2\"}]"
+        );
     }
 }
--- src/json_writer/json_object_writer.rs
+++ src/json_writer/json_object_writer.rs
@@ -1,17 +1,26 @@
-use super::JsonObject;
+use super::{JsonArrayWriter, JsonObject};
 
 pub struct JsonObjectWriter {
     first_element: bool,
-    raw: Vec<u8>,
+    raw: Option<Vec<u8>>,
 }
 
 impl JsonObjectWriter {
     pub fn new() -> Self {
         let mut raw = Vec::new();
         raw.push(b'{');
+
         Self {
-            raw,
             first_element: true,
+            raw: Some(raw),
+        }
+    }
+
+    pub(crate) fn new_as_nested(mut raw: Vec<u8>) -> Self {
+        raw.push(b'{');
+        Self {
+            first_element: true,
+            raw: Some(raw),
         }
     }
 
@@ -23,30 +32,69 @@ impl JsonObjectWriter {
         if self.first_element {
             self.first_element = false;
         } else {
-            self.raw.push(b',');
+            self.raw.as_mut().unwrap().push(b',');
         }
     }
 
     fn write_key(&mut self, key: &str) {
-        self.raw.push(b'"');
+        let raw = self.raw.as_mut().unwrap();
+        raw.push(b'"');
 
-        crate::json_string_value::write_escaped_json_string_value(key, &mut self.raw);
-        self.raw.extend_from_slice("\":".as_bytes());
+        crate::json_string_value::write_escaped_json_string_value(key, raw);
+        raw.extend_from_slice("\":".as_bytes());
+    }
+
+    pub fn write_json_object(
+        &mut self,
+        key: &str,
+        write_object: impl Fn(&mut JsonObjectWriter) -> (),
+    ) {
+        self.add_delimiter();
+        self.write_key(key);
+
+        let raw = self.raw.take().unwrap();
+
+        let mut json_object_writer = Self::new_as_nested(raw);
+
+        write_object(&mut json_object_writer);
+
+        let raw = json_object_writer.build();
+        self.raw = Some(raw);
+    }
+
+    pub fn write_json_array(
+        &mut self,
+        key: &str,
+        write_array: impl Fn(&mut JsonArrayWriter) -> (),
+    ) {
+        self.add_delimiter();
+        self.write_key(key);
+
+        let raw = self.raw.take().unwrap();
+
+        let mut json_array_writer = JsonArrayWriter::new_as_nested(raw);
+
+        write_array(&mut json_array_writer);
+
+        let raw = json_array_writer.build();
+        self.raw = Some(raw);
     }
 
     pub fn write(&mut self, key: &str, value: impl JsonObject) {
         self.add_delimiter();
         self.write_key(key);
-        value.write_into(&mut self.raw);
+
+        value.write_into(self.raw.as_mut().unwrap());
     }
 
     pub fn build(mut self) -> Vec<u8> {
-        self.raw.push(b'}');
-        self.raw
+        let mut raw = self.raw.take().unwrap();
+        raw.push(b'}');
+        raw
     }
 
     pub fn build_into(&self, dest: &mut Vec<u8>) {
-        dest.extend_from_slice(&self.raw);
+        dest.extend_from_slice(self.raw.as_ref().unwrap());
         dest.push(b'}');
     }
 }
@@ -74,5 +122,48 @@ mod tests {
             "{\"key\":\"value\",\"key2\":\"\\'value\\'\"}",
             std::str::from_utf8(&result).unwrap()
         );
+    }
+
+    #[test]
+    fn test_nested_object_write() {
+        let mut json_writer = super::JsonObjectWriter::new();
+
+        json_writer.write("key1", "value1");
+
+        json_writer.write_json_object("key2", |json_writer| {
+            json_writer.write("key3", "value3");
+            json_writer.write("key4", 54);
+        });
+
+        let result = json_writer.build();
+
+        let result = String::from_utf8(result).unwrap();
+
+        println!("{}", result);
+    }
+
+    #[test]
+    fn test_nested_array_write() {
+        let mut json_writer = super::JsonObjectWriter::new();
+
+        json_writer.write("key1", "value1");
+
+        json_writer.write_json_array("key2", |json_array_writer| {
+            json_array_writer.write_json_object(|json_object| {
+                json_object.write("key3", "value3");
+                json_object.write("key4", 54);
+            });
+
+            json_array_writer.write_json_object(|json_object| {
+                json_object.write("key3", "value5");
+                json_object.write("key4", 55);
+            });
+        });
+
+        let result = json_writer.build();
+
+        let result = String::from_utf8(result).unwrap();
+
+        assert_eq!("{\"key1\":\"value1\",\"key2\":[{\"key3\":\"value3\",\"key4\":54},{\"key3\":\"value5\",\"key4\":55}]}", result.as_str());
     }
 }
